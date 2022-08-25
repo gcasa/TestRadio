f0d281630ee4f486b8f18f79ad046551bab9665e
Gregory John Casamento <greg.casamento@gmail.com>
2022-08-25 13:22:04 -0400

    Initial Commit

---
+++ TestRadio/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  TestRadio
+//
+//  Created by Gregory Casamento on 8/25/22.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ TestRadio/AppDelegate.m
@@ -0,0 +1,32 @@
+//
+//  AppDelegate.m
+//  TestRadio
+//
+//  Created by Gregory Casamento on 8/25/22.
+//
+
+#import "AppDelegate.h"
+
+@interface AppDelegate ()
+
+@property (strong) IBOutlet NSWindow *window;
+@end
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
+    // Insert code here to initialize your application
+}
+
+
+- (void)applicationWillTerminate:(NSNotification *)aNotification {
+    // Insert code here to tear down your application
+}
+
+
+- (BOOL)applicationSupportsSecureRestorableState:(NSApplication *)app {
+    return YES;
+}
+
+
+@end
